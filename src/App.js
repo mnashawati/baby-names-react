b527c7f034287e2399da47650252012f3e034569
Mo Nashawati <mo.nashawati@gmail.com>
2020-05-29 01:27:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,20 +19,17 @@ function App() {
     return "";
   }
 
-  // function handelSearch(e) {
-  //   const searchTerm = e.target.value;
-  // }
-
   const filterAndUpdateCurrentNames = (nameObjects) => {
-    let searchResult = nameObjects.filter((obj) =>
+    const searchResult = nameObjects.filter((obj) =>
       obj.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
     return searchResult;
   };
 
-  // function checkGender(array) {
-  //   if  array.filter((element) => element.sex === "m");
-  // }
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    setCurrentNames(filterAndUpdateCurrentNames(currentNames));
+  };
 
   return (
     <div className="App">
@@ -40,14 +37,7 @@ function App() {
         className="search-input"
         placeholder="search names"
         value={searchTerm}
-        onChange={(e) => {
-          console.log(e.target.value);
-          setCurrentNames(() => filterAndUpdateCurrentNames(sortedNames));
-          setSearchTerm(e.target.value);
-
-          // debugger;
-          // filterAndUpdateCurrentNames(sortedNames);
-        }}
+        onChange={handleSearch}
       ></input>
       <div className="radio male-names">
         <label>
